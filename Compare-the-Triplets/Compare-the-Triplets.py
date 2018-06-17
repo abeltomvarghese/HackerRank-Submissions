a1b5766a6333ae3761ff59c5b32c7a254b2d7149
Abel Tom Varghese <abeltomvarghese1998@gmail.com>
2018-06-17 17:12:42 +0100

    fixed the output string

--- Compare-the-Triplets/Compare-the-Triplets.py
+++ Compare-the-Triplets/Compare-the-Triplets.py
@@ -6,7 +6,7 @@ import random
 import re
 import sys
 
-# Complete the solve function below.
+
 def solve(a, b):
     alice = 0 
     bob = 0
@@ -15,19 +15,20 @@ def solve(a, b):
             bob +=1
         elif a[x] > b[x]:
             alice +=1
-    result = (str(alice),str(bob))
+    result = (str(alice)+ " " + str(bob))
     return result
 
 if __name__ == '__main__':
-    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    
 
     a = list(map(int, input().rstrip().split()))
 
     b = list(map(int, input().rstrip().split()))
 
     result = solve(a, b)
+  
+    
+    
+    
     print(result)
-    #fptr.write(' '.join(map(str, result)))
-    #fptr.write('\n')
-
-    #fptr.close()
+    
